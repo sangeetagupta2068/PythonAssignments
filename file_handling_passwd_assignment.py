b71e20b695b1f1a309190b5aad9b2b07982a7a47
sangeetagupta <tinnigupta1998@gmail.com>
2019-07-29 15:44:30 +0530

    created separate output file for output.

--- file_handling_passwd_assignment.py
+++ file_handling_passwd_assignment.py
@@ -4,7 +4,7 @@
     created by: Sangeeta Gupta
     created on: 28th July 2019
     objective: This program retrieves, prints all the system users username from the /etc/passwd file on the console
-               and writes retrieved usernames on a file namely passwd_username.txt
+               and writes all retrieved usernames on a file namely passwd_username.txt
     Additional Info: The /etc/passwd in Mac OS contains comments describing that the file is only consulted when
                      system is running on single user mode. Else, user information is provided by Open directory.
                      Hence, on line 1 a check is provided in order to distinguish the comment from user information.
